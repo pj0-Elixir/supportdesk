121449572d8afef23f801264adbeedd3d6929179
PJ0_EliXir <paujoe200418@gmail.com>
2025-06-13 13:31:11 +0100

    Basic Express Server setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+console.log("🔄 Server file reloaded...");
 const express = require('express')
 
 const dotenv = require('dotenv').config()
@@ -8,4 +9,11 @@ const app = express()
 app.get('/', (req, res)=>{
      res.status(200).json({message: 'Welcome to Support Desk API'}) 
 })
+
+app.post('/api/users/', (req, res)=>{
+     res.status(200).json({message: 'New user created'}) 
+})
+
+
+
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
