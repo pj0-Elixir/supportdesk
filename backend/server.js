6fd2f9456855ffeff5953139a528115cc49b84db
pj0-Elixir <paujoe200418@gmail.com>
2026-04-10 14:16:37 +0100

    cors problem update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(cors({
-    origin: 'https://supportdex.netlify.app', 
+    origin: 'https://supportdesk-frontend.netlify.app', 
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, 
 }));
