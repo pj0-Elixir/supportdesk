e7ceddd71f12f467bafc720b442155f4043d949d
pj0-Elixir <paujoe200418@gmail.com>
2026-04-13 12:53:41 +0100

    fix: note service API url

--- frontend/src/features/note/noteService.js
+++ frontend/src/features/note/noteService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/tickets/'
+const API_URL = `${process.env.REACT_APP_API_URL}/api/tickets/`;
 
 //  get tickets note
 const getNotes = async (ticketId, token) => {
